e06de564b342b47a158e3f0f02985d0d2c01a807
Sami Utriainen <samiutri@gmail.com>
2020-01-10 07:49:29 +0200

    more python

--- python/src/connector.py
+++ python/src/connector.py
@@ -1,23 +1,24 @@
 from __future__ import print_function
 
-import random
-import logging
-
 import grpc
 
 import connector_service_pb2
 import connector_service_pb2_grpc
 
+import os.path
+from os import path
+
+
 class Connector(object):
-    
     log = None
 
     def __init__(self, host, port):
         self._channel = grpc.insecure_channel('%s:%d' % (host, port))
         self._blockingStub = connector_service_pb2_grpc.ConnectorServiceStub(self._channel)
 
-    def _log(self, level, message):
-        if (Connector.log != None):
+    @staticmethod
+    def _log(level, message):
+        if Connector.log is not None:
             Connector.log(level, message)
 
     def shutdown(self):
@@ -30,9 +31,10 @@ class Connector(object):
         except grpc.RpcError as e:
             self._log(2, 'RPC failed: %s' % str(e))
         return value
-   
-    def startRun(self, run_name, set_name, project, version, primary_device_serial, secondary_device_serial, profiling_settings, tags):
-        
+
+    def start_run(self, run_name, set_name, project, version, primary_device_serial, secondary_device_serial,
+                  profiling_settings, tags):
+
         request = connector_service_pb2.StartRunRequest()
         request.run_name = run_name or ''
         request.set_name = set_name or ''
@@ -42,21 +44,101 @@ class Connector(object):
         request.secondary_device_serial = secondary_device_serial or ''
         request.profiling_settings = profiling_settings or ''
         request.tags.update(tags or {})
-        
         try:
             response = self._blockingStub.StartRun(request)
-            return response.run_id;
+            return response.run_id
         except grpc.RpcError as e:
             self._log(2, 'RPC failed: %s' % str(e))
-        return None  
-    
+        return None
+
+    def on_use_case_start(self, run_id, use_case_name, use_case_id, target_process, requirement_id):
+
+        if run_id is None or len(run_id.strip()) == 0:
+            return False
+
+        use_case_name_invalid = use_case_name is None or len(use_case_name.strip()) == 0
+        use_case_id_invalid = use_case_id is None or len(use_case_id.strip()) == 0
+
+        if use_case_name_invalid and use_case_id_invalid:
+            return False
+
+        request = connector_service_pb2.UseCaseStartRequest()
+        request.run_id = run_id
+        request.use_case_name = use_case_name or ''
+        request.use_case_id = use_case_id or ''
+        request.target_process = target_process or ''
+        request.requirement_id = requirement_id or ''
+        try:
+            self._blockingStub.OnUseCaseStart(request)
+            return True
+        except grpc.RpcError as e:
+            self._log(2, 'RPC failed: %s' % str(e))
+        return False
+
+    def on_use_case_end(self, run_id, result, active_run_time, fail_cause, reset_intended):
+
+        if run_id is None or len(run_id.strip()) == 0:
+            return False
+
+        request = connector_service_pb2.UseCaseEndRequest()
+        request.run_id = run_id
+        request.result = result
+        request.activeRunTime = active_run_time
+        request.fail_cause = fail_cause or ''
+        request.reset_intended = reset_intended
+        try:
+            self._blockingStub.OnUseCaseEnd(request)
+            return True
+        except grpc.RpcError as e:
+            self._log(2, 'RPC failed: %s' % str(e))
+        return False
+
+    def on_log_step(self, run_id, step_name, result, screenshot):
+
+        take_screenshot = isinstance(screenshot, bool) and screenshot is True
+        screenshot_bytes = None
+        if isinstance(screenshot, bytes) and len(screenshot):
+            screenshot_bytes = screenshot
+        elif isinstance(screenshot, str):
+            pass
+
+        pass
+
+    def _on_log_step(self, run_id, step_name, result, take_screenshot, screenshot_bytes):
+
+        if run_id is None or len(run_id.strip()) == 0:
+            return False
+
+        if not take_screenshot and (not screenshot_bytes or len(screenshot_bytes) == 0) and (step_name is None or len(step_name.strip()) == 0):
+            return False
+
+        request = connector_service_pb2.LogStepRequest()
+        request.run_id = run_id
+        request.step_name = step_name or ''
+        request.result = result
+        request.take_screenshot = take_screenshot
+        request.screenshot_bytes = screenshot_bytes
+        try:
+            self._blockingStub.OnLogStep(request)
+            return True
+        except grpc.RpcError as e:
+            self._log(2, 'RPC failed: %s' % str(e))
+        return False
+
+
+def get_bytes_from_file(filename):
+    if filename is not None and path.exists(filename):
+        try:
+            return open(filename, "rb").read()
+        except Exception as e:
+            pass
 
 def cw(level, message):
     print(message)
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     Connector.log = cw
     connector = Connector('localhost', 31321)
-    print(connector.startRun('python', 'demo', 'PProject', '1.0.0', None, None, None, None))
+    print(connector.start_run('python', 'demo', 'PProject', '1.0.0', None, None, None, None))
     connector.shutdown()
