f780846e9a6b57ea301446ddafb8b028dcd68979
Sami <sami.utriainen@profilence.com>
2020-01-12 19:49:18 +0200

    python docstrings

--- python/src/connector.py
+++ python/src/connector.py
@@ -11,42 +11,85 @@ import wrappers_pb2
 import empty_pb2
 
 from os import path
+import sys
+import re
 
 __copyright__ = "Copyright 2020 Profilence"
 __license__ = "Apache License, Version 2.0"
 
 
 class TestRequestListenerBase(object):
+    """ Base class for test request handling """
 
     def on_error(self, e):
+        """ Called if error occurs while listening test requests
+
+        Parameters:
+            e (Exception): The exception object
+
+        """
+
         pass
 
     def on_completed(self):
+        """ Called when the asynchronous sequence completes """
+
         pass
 
     def on_test_start_requested(self, request):
+        """ Called by service for starting a new test in the test node (this client)
+
+         Parameters:
+             request (TestStartRequest): Start request details
+
+         """
+
         pass
 
     def on_test_stop_requested(self, request):
+        """ Called by server for stopping ongoing test in the test node (this client)
+
+         Parameters:
+             request (TestStopRequest): Stop request details
+
+         """
         pass
 
 
 class Connector(object):
-    log = None
+    """ Driver class for connecting to Profilence ZETA test service """
 
-    def __init__(self, host, port):
-        self._channel = grpc.insecure_channel('%s:%d' % (host, port))
-        self._blockingStub = connector_service_pb2_grpc.ConnectorServiceStub(self._channel)
+    log = None
 
     @staticmethod
     def _log(level, message):
         if Connector.log is not None:
             Connector.log(level, message)
 
+    def __init__(self, host, port):
+        """ Creates a new Connector instance and connects to the service
+
+        Parameters:
+            host: The service address
+            port: The service port
+        """
+
+        self._channel = grpc.insecure_channel('%s:%d' % (host, port))
+        self._blockingStub = connector_service_pb2_grpc.ConnectorServiceStub(self._channel)
+
     def shutdown(self):
+        """ Shuts down the connection """
+
         self._channel.close()
 
     def ping(self):
+        """ Pings the service
+
+        Returns:
+            True on success; False otherwise
+
+        """
+
         value = 0
         try:
             value = self._blockingStub.Ping(connector_service_pb2.PingMessage()).result
@@ -56,6 +99,22 @@ class Connector(object):
 
     def start_run_with_recommended_settings(self, run_name, set_name, project, version, primary_device_serial,
                                             secondary_device_serial, tags):
+        """ Requests the service for a new test run with recommended profiling settings
+
+        Parameters:
+            run_name (str):                 Name of the test run
+            set_name (str):                 Name of the test set
+            project (str):                  Name of the project under test
+            version (str):                  Version of the project
+            primary_device_serial (str):    Identifier of the primary DUT
+            secondary_device_serial (str):  Identifier of the secondary DUT
+            tags (dict):                    Tags for the test run
+
+        Returns:
+            Test run id (str) on success; None otherwise
+
+        """
+
         return self.start_run(run_name,
                               set_name,
                               project,
@@ -67,6 +126,22 @@ class Connector(object):
 
     def start_run(self, run_name, set_name, project, version, primary_device_serial, secondary_device_serial,
                   profiling_settings, tags):
+        """ Requests the service for a new test run
+
+        Parameters:
+            run_name (str):                         Name of the test run
+            set_name (str):                         Name of the test set
+            project (str):                          Name of the project under test
+            version (str):                          Version of the project
+            primary_device_serial (str):            Identifier of the primary DUT
+            secondary_device_serial (str):          Identifier of the secondary DUT
+            profiling_settings (str|TextIOBase):    Profiling settings as JSON string, or a file handle to JSON file
+            tags (dict):                            Tags for the test run
+
+        Returns:
+            Test run id (str) on success; None otherwise
+
+        """
 
         if profiling_settings and isinstance(profiling_settings, TextIOBase):
             try:
@@ -92,6 +167,19 @@ class Connector(object):
         return None
 
     def on_use_case_start(self, run_id, use_case_name, use_case_id, target_process, requirement_id):
+        """ Called to notify the service about start of a new use/test case
+
+        Parameters:
+            run_id (str):           ID of the test run
+            use_case_name (str):    Name of the use/test case
+            use_case_id (str):      ID of the use/test case
+            target_process (str):   Name of a process to monitor more closely during the use/test case
+            requirement_id (str):   ID of requirement this use/test case verifies
+
+        Returns:
+            True is notification sent successfully; otherwise False
+
+        """
 
         if run_id is None or len(run_id.strip()) == 0:
             return False
@@ -116,6 +204,19 @@ class Connector(object):
         return False
 
     def on_use_case_end(self, run_id, result, active_run_time, fail_cause, reset_intended):
+        """ Called to notify the service about end of the use/test case
+
+        Parameters:
+            run_id (str):               ID of the test run
+            result (bool):              Result of the use/test case (True=Pass; False=Failure)
+            active_run_time (long):     Time (milliseconds) used active testing during the test
+            fail_cause (str):           Fail cause, if any
+            reset_intended (bool):      True if a DUT reset was caused by the test; otherwise False
+
+        Returns:
+            True is notification sent successfully; otherwise False
+
+        """
 
         if run_id is None or len(run_id.strip()) == 0:
             return False
@@ -134,6 +235,21 @@ class Connector(object):
         return False
 
     def on_log_step(self, run_id, step_name, result, screenshot):
+        """ Called to notify the service about a new step within the use/test case
+
+        Parameters:
+            run_id (str):                   ID of the test run
+            step_name (str):                Step name/description
+            result (bool):                  True if the step passed as expected; otherwise False
+            screenshot (bool|bytes|str):    True for requesting the service to take a screenshot;
+                                            image bytes or path to image file for sending a local screenshot for
+                                            the service. If requesting the service to take a screenshot, make sure
+                                            it's supported for the DUT platform by the service.
+
+        Returns:
+            True is notification sent successfully; otherwise False
+
+        """
 
         take_screenshot = isinstance(screenshot, bool) and screenshot is True
         screenshot_bytes = None
@@ -169,6 +285,16 @@ class Connector(object):
         return False
 
     def log_trace(self, run_id, data):
+        """ Send a line or internal/execution log to the service
+
+        Parameters:
+            run_id (str):   ID of the test run
+            data (str):     Log line
+
+        Returns:
+            True is notification sent successfully; otherwise False
+
+        """
 
         if run_id is None or len(run_id.strip()) == 0:
             return False
@@ -186,7 +312,17 @@ class Connector(object):
             self._log(2, 'RPC failed: %s' % str(e))
         return False
 
-    def stop_run(self, run_id, discard_results):
+    def stop_run(self, run_id, discard_results=False):
+        """ Requests the service to stop an ongoing test run
+
+        Parameters:
+            run_id:             ID of the test run
+            discard_results:    True to discard al the test data
+
+        Returns:
+            True is notification sent successfully; otherwise False
+
+        """
 
         if run_id is None or len(run_id.strip()) == 0:
             return False
@@ -201,7 +337,16 @@ class Connector(object):
             self._log(2, 'RPC failed: %s' % str(e))
         return False
 
+    # For implementing a test node for the test cloud -->
+
     def subscribe_to_test_requests(self, listener):
+        """ Subscribe to test requests
+
+        Parameters:
+            listener (TestRequestListenerBase): Listener handler for asynchronous requests
+
+        """
+
         if isinstance(listener, TestRequestListenerBase):
             threading.Thread(target=self._subscribe_to_test_requests, args=[listener], daemon=False).start()
 
@@ -213,7 +358,7 @@ class Connector(object):
                     if request and hasattr(request, 'request_type'):
                         try:
                             rt = request.request_type
-                            if rt is 1:
+                            if rt == 1:
                                 start_req = connector_service_pb2.TestStartRequest()
                                 start_req.ParseFromString(request.payload)
                                 listener.on_test_start_requested(start_req)
@@ -229,6 +374,19 @@ class Connector(object):
                 self._log(2, 'RPC failed: %s' % str(e))
 
     def respond_to_test_request(self, run_id, status, result, fail_cause, log):
+        """ Send response to a test request
+
+        Parameters:
+            run_id (str):         ID of the local test run
+            status (str):         Status
+            result (bool):        True if test run was executed without issues
+            fail_cause (str):     Fail cause if any
+            log (list[str]):      Tool log
+
+        Returns:
+            True if response was sent to service; otherwise False
+
+        """
 
         if run_id is None or len(run_id.strip()) == 0:
             return False
@@ -248,6 +406,18 @@ class Connector(object):
         return False
 
     def add_node(self, node_id, pool, node_type, variables):
+        """ Establishes a test node for the service (test farm)
+
+        Parameters:
+            node_id (str):      Unique ID of the node
+            pool (str):         Name of the pool/group this node belongs to
+            node_type (str):    Type of the node
+            variables (str):    Variables
+
+        Return:
+            True if request was successfully sent to service; otherwise False
+
+        """
 
         if node_id is None or len(node_id.strip()) == 0:
             return False
@@ -265,6 +435,15 @@ class Connector(object):
         return False
 
     def remove_node(self, node_id):
+        """ Removes the test node from the service (test farm)
+
+        Parameters:
+            node_id:    Unique ID of the node
+
+        Returns:
+            True if request was successfully sent to service; otherwise False
+
+        """
 
         if node_id is None or len(node_id.strip()) == 0:
             return False
@@ -279,6 +458,21 @@ class Connector(object):
         return False
 
     def update_node(self, node_id, current_use_case, run_state, current_run_id, node_state, pool, variables):
+        """ Updates the service with information from an existing test node
+
+        Parameters:
+            node_id (str):              Unique ID of the node
+            current_use_case (str):     Name of current use case
+            run_state (str):            State of the run
+            current_run_id (str):       ID of the current run
+            node_state (str):           State of the node
+            pool (str):                 Name of the pool/group this node belongs to
+            variables (str):            Variables
+
+        Returns:
+            True if request was successfully sent to service; otherwise False
+
+        """
 
         if node_id is None or len(node_id.strip()) == 0:
             return False
@@ -310,6 +504,7 @@ class Connector(object):
         return False
 
 
+# noinspection PyBroadException
 def get_bytes_from_file(filename):
     if filename is not None and path.exists(filename):
         try:
@@ -318,12 +513,38 @@ def get_bytes_from_file(filename):
             pass
 
 
-def cw(level, message):
-    print(message)
-
-
 if __name__ == '__main__':
-    Connector.log = cw
-    connector = Connector('localhost', 31321)
-    print(connector.start_run('python', 'demo', 'PProject', '1.0.0', None, None, None, None))
-    connector.shutdown()
+
+    if len(sys.argv):
+        args = sys.argv[1:]
+        p = re.compile('([^:]+):([0-9]+)')
+
+        m = None
+        address = None
+        port = None
+
+        if len(args) >= 1 and (m := p.match(args[0])):
+            address = m.group(1)
+            port = int(m.group(2))
+        elif len(args) >= 2 and re.search('[0-9]+', args[1]):
+            address = args[0]
+            port = int(args[1])
+        else:
+            print('Invalid parameters; give address and port for ping test: [address]:[port]')
+
+        Connector.log = lambda x, y: print(y)
+        print('Connecting to %s:%d ...' % (address, port))
+        client = None
+        try:
+            client = Connector(address, port)
+            print('Pinging ..')
+            if client.ping() == 1:
+                print('Successfully pinged the service')
+            else:
+                print('Failed to ping the service')
+        finally:
+            if client:
+                print('Shutting down ...')
+                client.shutdown()
+    else:
+        print('Invalid parameters; give address and port for ping test: [address]:[port]')
