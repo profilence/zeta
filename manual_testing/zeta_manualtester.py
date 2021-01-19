9bbd01819ba0e8e12538fa4a9beb10bdc6036653
Akseli Lahtinen <akseli.lahtinen@profilence.com>
2021-01-19 11:29:01 +0200

    json support to manualtester

--- manual_testing/zeta_manualtester.py
+++ manual_testing/zeta_manualtester.py
@@ -1,6 +1,7 @@
 import argparse
 import time
 import datetime
+import json
 from connector import Connector
 from connector_types import TestType
 
@@ -56,34 +57,10 @@ class ZetaManualTester():
                 return {"true":True,"false":False,"y":True,"n":False}[input(prompt).lower()]
             except KeyError:
                 print("Invalid input please enter True or False!")
-    
-    def ask_input(self, variable, prompt):
-        if variable == "":
-            return input(prompt)
-
-    #setup
-    def setup(self):
-        print("===Setup===")
-        self.project_name = self.ask_input(variable=self.project_name,prompt="Project name: ")
-        self.project_version = self.ask_input(variable=self.project_version, prompt="Project version (leave empty to use default): ")
-        self.target_device_identifier = self.ask_input(variable=self.target_device_identifier, prompt="Target device identifier: ")
-        self.target_device_type = self.ask_input(variable=self.target_device_type, prompt="Target device type (android/ios/etc..): ")
-        self.secondary_device_identifier = self.ask_input(variable=self.secondary_device_identifier, prompt="Secondary device identifier (if any): ")
-        if self.secondary_device_identifier == "":
-            self.secondary_device_identifier = None
-            self.secondary_device_type = None
-        else:
-            self.secondary_device_type = self.ask_input(variable=self.secondary_device_identifier, prompt="Secondary device type (if any, android/ios/etc..): ")
-        self.profiling_settings_path = self.ask_input(variable=self.profiling_settings_path, prompt="Path to profiling settings, if any (example: /home/tests/profiling_settings.json): ")
 
     #usecase loop
     def usecase_loop(self, test_run_id):
         print("===Usecase===")
-        self.use_case_name = self.ask_input(variable=self.use_case_name, prompt="Use case name: ")
-        self.use_case_id = self.ask_input(variable=self.use_case_id, prompt="Use case id: ")
-        self.use_case_group = self.ask_input(variable=self.use_case_group, prompt="Use case group: ")
-        self.target_process_name = self.ask_input(variable=self.target_process_name, prompt="Target process name: ")
-        self.requirement_id = self.ask_input(variable=self.requirement_id, prompt="Requirement id: ")
         client.log_trace(test_run_id, "Starting use case %s" % self.use_case_name)
         start_time = datetime.datetime.now()
         client.on_use_case_start(test_run_id, self.use_case_name, self.use_case_id, self.use_case_group, self.test_set_name, TestType.NORMAL, self.target_process_name, self.requirement_id)
@@ -114,10 +91,11 @@ class ZetaManualTester():
     #main test loop
     def main_loop(self):
         print("===Starting new test run===")
-        self.test_run_name = self.ask_input(variable=self.test_run_name, prompt="Test run name: ")
-        self.test_set_name = self.ask_input(variable=self.test_set_name, prompt="Test set name: ")
-
-        test_run_id = client.start_run(self.test_run_name, self.test_set_name, self.project_name, self.project_version, self.target_device_identifier, self.target_device_type, self.secondary_device_identifier, self.secondary_device_type, open(self.profiling_settings_path), None, "")
+        self.test_run_name =str(time.time())+"-"+self.test_run_name
+        profiling_settings = None
+        if (self.profiling_settings_path != ""):
+            profiling_settings = open(self.profiling_settings_path)
+        test_run_id = client.start_run(self.test_run_name, self.test_set_name, self.project_name, self.project_version, self.target_device_identifier, self.target_device_type, self.secondary_device_identifier, self.secondary_device_type, profiling_settings, None, "")
 
         if test_run_id:
             run_test = True
@@ -137,42 +115,58 @@ class ZetaManualTester():
 #parameters for setup part + run + set
 if __name__ == "__main__":
     
-    parser = argparse.ArgumentParser()
+    #setup
 
-    parser.add_argument("-address", help="Connector address", metavar="address", default="localhost")
-    parser.add_argument("-port", help="Connector port", type=int, metavar="port", default=31321)
-    parser.add_argument("-test_run_name", help="Test run name", default="")
-    parser.add_argument("-test_set_name", help="Test set name", default="")
-    parser.add_argument("-project_name", help="Project name", default="")
-    parser.add_argument("-project_version", help="Project version, leave empty to use default", default=None)
-    parser.add_argument("-target_device_identifier", help="Target device identifier", default="")
-    parser.add_argument("-target_device_type", help="Target device type (android by default)", default="android")
-    parser.add_argument("-secondary_device_identifier", help="Secondary device identifier (if any)", default=None)
-    parser.add_argument("-secondary_device_type", help="Secondary device type (if any)", default=None)
-    parser.add_argument("-profiling_settings_path", help="Path to profiling settings", default="")
-    parser.add_argument("-use_case_name", help="Use case name", default="")
-    parser.add_argument("-use_case_id", help="Use case id", default="")
-    parser.add_argument("-use_case_group", help="Use case group", default="")
-    parser.add_argument("-target_process_name", help="Target process name", default="")
-    parser.add_argument("-requirement_id", help="Requirement id", default="")
+    #read json
+    f = open("manualtest_settings.json")
+    data = json.loads(f.read())
+    f.close()
 
-    
+    #parse arguments
+    parser = argparse.ArgumentParser(description="Script for using Zeta with manual testing. Fill manualtest_settings.json with necessary information. "+
+                                                "You can use following parameters to override the json file if necessary. For custom address and port, use the parameters.")
+
+    parser.add_argument("-address", help="Connector address (default: localhost)", metavar="address", default="localhost")
+    parser.add_argument("-port", help="Connector port (default: 31321)", type=int, metavar="port", default=31321)
+    parser.add_argument("-test_run_name", help="Test run name")
+    parser.add_argument("-test_set_name", help="Test set name")
+    parser.add_argument("-project_name", help="Project name")
+    parser.add_argument("-project_version", help="Project version, leave empty to use default")
+    parser.add_argument("-target_device_identifier", help="Target device identifier")
+    parser.add_argument("-target_device_type", help="Target device type (android by default)")
+    parser.add_argument("-secondary_device_identifier", help="Secondary device identifier (if any)")
+    parser.add_argument("-secondary_device_type", help="Secondary device type (if any)")
+    parser.add_argument("-profiling_settings_path", help="Path to profiling settings")
+    parser.add_argument("-use_case_name", help="Use case name")
+    parser.add_argument("-use_case_id", help="Use case id")
+    parser.add_argument("-use_case_group", help="Use case group")
+    parser.add_argument("-target_process_name", help="Target process name")
+    parser.add_argument("-requirement_id", help="Requirement id")
+
     args = parser.parse_args()
 
-    zeta = ZetaManualTester(test_run_name=args.test_run_name, 
-                            test_set_name=args.test_set_name, project_name=args.project_name, 
-                            project_version=args.project_version, target_device_identifier=args.target_device_identifier, 
-                            target_device_type=args.target_device_type, secondary_device_identifier=args.secondary_device_identifier, 
-                            secondary_device_type=args.secondary_device_type, profiling_settings_path=args.profiling_settings_path, 
-                            use_case_name=args.use_case_name, use_case_id=args.use_case_id, use_case_group=args.use_case_group, 
-                            target_process_name=args.target_process_name, requirement_id=args.requirement_id)
+    #create new zeta manual tester based on args and json. if arg is not empty, use it instead of json.
+    zeta = ZetaManualTester(test_run_name=data['test_run_name'] if args.test_run_name == None else args.test_run_name, 
+                            test_set_name=data['test_set_name'] if args.test_set_name == None else args.test_set_name, 
+                            project_name=data['project_name'] if args.project_name == None else args.project_name, 
+                            project_version=data['project_version'] if args.project_version == None else args.project_version, 
+                            target_device_identifier=data['target_device_identifier'] if args.target_device_identifier == None else args.target_device_identifier, 
+                            target_device_type=data['target_device_type'] if args.target_device_type == None else args.target_device_type, 
+                            secondary_device_identifier=data['secondary_device_identifier'] if args.secondary_device_identifier == None else args.secondary_device_identifier, 
+                            secondary_device_type=data['secondary_device_type'] if args.secondary_device_type == None else args.secondary_device_type, 
+                            profiling_settings_path=data['profiling_settings_path'] if args.profiling_settings_path == None else args.profiling_settings_path, 
+                            use_case_name=data['use_case_name'] if args.use_case_name == None else args.use_case_name, 
+                            use_case_id=data['use_case_id'] if args.use_case_id == None else args.use_case_id, 
+                            use_case_group=data['use_case_group'] if args.use_case_group == None else args.use_case_group, 
+                            target_process_name=data['target_process_name'] if args.target_process_name == None else args.target_process_name, 
+                            requirement_id=data['requirement_id'] if args.requirement_id == None else args.requirement_id)
+
 
     #program
     try:
         client = Connector(args.address, args.port)
         if client.ping() == 1:
             print("Connection to Zeta succesful!")
-            zeta.setup()
             zeta.main_loop()
         else:
             print("Connection to Zeta failed. Check address and port")
