c4782fe99e5259861b27f152e27ba735f9bebdc7
Akseli Lahtinen <akseli.lahtinen@profilence.com>
2021-05-03 11:55:31 +0300

    update java example to ping the test run

--- examples/java/Example.java
+++ examples/java/Example.java
@@ -6,10 +6,11 @@ import java.util.Random;
 import com.profilence.zeta.Connector;
 import com.profilence.zeta.ILogger;
 import com.profilence.zeta.LogLevel;
+import com.profilence.zeta.LogStepRequest;
 import com.profilence.zeta.PingResponseType;
 import com.profilence.zeta.TestType;
 
-public class ClientExample {
+public class Example {
 
     public static void main(String[] args) throws InterruptedException {
         
@@ -22,7 +23,10 @@ public class ClientExample {
             }
         });
         
-        Connector client = new Connector("localhost", 31321);
+
+        
+        Connector client = new Connector("127.0.0.1", 31312);
+        Thread t = null;
         
         try {
         
@@ -30,7 +34,8 @@ public class ClientExample {
             print(response);
             
             if (response == PingResponseType.Ok) {
-              
+            	
+
                 String testRunName = "demo";
                 String testSetName = "dummy";
                 String projectName = "AProject";
@@ -38,11 +43,16 @@ public class ClientExample {
                 String targetDeviceIdentifier = "emulator-5554";
                 String targetDeviceType = "android";
                 File profilingSettings = new File("/home/tests/profiling_settings.json");
-              
+
                 String testRunID = client.startRun(testRunName, testSetName, projectName, projectVersion, targetDeviceIdentifier, targetDeviceType, null, null, profilingSettings, null);
                 print("testRunID: " + testRunID);
                 if (testRunID != null) {
-                    for(int i = 1; i <= 10; i++) {
+                	
+                    RunPinger runPinger = new RunPinger(client, testRunID);
+                    t = new Thread(runPinger);
+                    t.start();
+                	
+                    for(int i = 1; i <= 3; i++) {
                         String useCaseName = "use case " + i;
                         String useCaseID = "id_" + i;
                         client.logTrace(testRunID, "Starting use case " + useCaseName + " from java");
@@ -53,7 +63,7 @@ public class ClientExample {
                                 print("case: " + i + "; " + stepName);
                                 boolean stepPassed = j % 5 == 0;
                                 File screenShot = stepPassed == false ? new File("/home/tests/resources/dummy.png") : null;
-                                client.onLogStep(testRunID, stepName, stepPassed, screenShot);
+                                client.onLogStep(testRunID, stepName, LogStepRequest.StepType.Running, stepPassed, screenShot);
                                 Thread.sleep(1000);
                             }
                         } catch(Exception e) {
@@ -65,14 +75,52 @@ public class ClientExample {
                         client.onUseCaseEnd(testRunID, failed, activeRunTime, cause, intendedReset);
                     }
                     print("Successfully stopped test run: " + client.stopRun(testRunID, false));
+                    runPinger.running = false;
                 }
             }
-        } finally {
+        } 
+        finally 
+        {
           client.shutdown();
+          t.join();
         }
     }
     
+
     public static void print(Object obj) {
         System.out.println(obj);
     }
+    
+    //Pings the testrun to keep it alive
+    public static class RunPinger implements Runnable {
+
+    	public boolean running = true;
+        Connector client = null;
+        String testRunID = "";
+
+        public RunPinger(Connector client, String testRunID) {
+            this.client = client;
+            this.testRunID = testRunID;
+        }
+
+        public void run() {
+        	
+        	while (running)
+        	{	
+        		try 
+        		{
+        			boolean returnval = client.pingRun(testRunID);
+        			print("Pinging test run: " + String.valueOf(returnval));
+					Thread.sleep(30000);
+				} 
+        		catch (InterruptedException e) 
+        		{
+					e.printStackTrace();
+				}
+        	}
+            
+        }
+    }
+
+
 }
